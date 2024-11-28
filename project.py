4d9b3e826d264699ac1e33a2a428428650a62998
= <=>
2024-11-27 23:00:23 -0500

    added code to connect to sql

--- project.py
+++ project.py
@@ -4,3 +4,19 @@
 
 # Lambert, tell me if this is how I update code to Git
 
+import sqlite3
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+#path to sql database
+database_path = 'C:\Users\deeks\OneDrive\Desktop\WFU_MSBA\Data Management\Spotify_Project.songs.db'
+
+#connec to sql db
+connection = sqlite3.connect(database_path)
+
+#create cursor object
+cursor = connection.cursor()
+
+#write sql query
